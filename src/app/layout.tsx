a1c0ab6875d757f333dc5e5d08425809167e56fa
milanzivanov <milan.zivanov@gmail.com>
2025-08-22 17:32:30 +0200

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head />
       <body>{children}</body>
     </html>
