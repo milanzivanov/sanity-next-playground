8a142d26f0e2d701a69060a2b62d66b3cb929ac1
milan <milanzivanov@users.noreply.github.com>
2025-08-25 13:06:31 +0200

    Initial commit from ZIP

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,16 @@
+import { SanityLive } from '@/sanity/lib/live'
+import { Header } from "@/components/Header";
+
+export default function FrontendLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <section className="bg-white min-h-screen">
+      <Header />
+      {children}
+      <SanityLive />
+    </section>
+  )
+}
